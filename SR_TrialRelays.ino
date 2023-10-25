885ff0fc94486809f307a7ae15fc645f297a87fe
AarushiDhanger <aarushi.dhanger@gmail.com>
2023-10-24 22:00:22 -0700

    Add files via upload

---
+++ SR_TrialRelays.ino
@@ -0,0 +1,23 @@
+const int relayOne = 3; //wash
+const int relayTwo = 4; //dry
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(relayOne, OUTPUT);
+  pinMode(relayTwo, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write("Relays = HIGH ");
+  digitalWrite(relayOne, HIGH); 
+  digitalWrite(relayTwo, HIGH);
+  delay(5000);
+
+  Serial.write("Relays = LOW ");
+  digitalWrite(relayOne, LOW); 
+  digitalWrite(relayTwo, LOW);
+  delay(5000);
+  
+}
