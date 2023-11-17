bb8a1b1ef7804916a4c879fe781a0e0e110ceab5
DavidWei2022 <102999460+DavidWei2022@users.noreply.github.com>
2023-11-17 11:35:02 -0800

    code finished with ui

--- SR_Final.ino
+++ SR_Final.ino
@@ -18,22 +18,17 @@ int washCount = 0;
 int dryCount = 0;
 
 /* max acceptable cycles */
-int maxWash = 100;
-int maxDry = 75;
+int maxWash = 10;
+int maxDry = 10;
 
 /* time constants for wash and dry duration */
-int washTime = 45000;
-int dryTime = 45000;
+int washTime = 30000;
+int dryTime = 30000;
 
 void setup() {
   // put your setup code here, to run once:
   lcd.init();
   lcd.backlight();
-
-  lcd.setCursor(0,0);  lcd.print("--------------------"); 
-  lcd.setCursor(0,1);  lcd.print("     WELCOME TO");
-  lcd.setCursor(0,2);  lcd.print("   SHOE - RIFFIC! ");
-  lcd.setCursor(0,3);  lcd.print("--------------------");
 
   pinMode(relayOne, OUTPUT);
   pinMode(relayTwo, OUTPUT);
@@ -45,71 +40,56 @@ void setup() {
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   lcd.clear();
+  welcome();
   delay(2000); 
-  resetScreen();
+  resetScreen(washCount, dryCount);
 
 }
 
 void loop() {
-  //  while (washCount < maxWash &&dryCount < maxDry){    
 
   if(digitalRead(resetPin) == LOW){
     lcd.setCursor(7,1); lcd.print("ON ");
-    lcd.clear();//clear screen to print new words
-    lcd.print("Reset Selected."); //2 Words, 15 characters 
     delay(2000);
     resetFunct();
     delay(2000);  
   }
   else{
      digitalRead(resetPin) == HIGH;
-    lcd.setCursor(0,2); lcd.print("Reset: OFF"); 
   }
        
 
   if(digitalRead(dryPin) == LOW){
-    lcd.setCursor(5,2); lcd.print("ON");
-    lcd.clear();//clear screen to print new words
-    lcd.print("Dry Selected."); //2 Words, 13 characters
+    lcd.setCursor(7,2); lcd.print("ON ");
     delay(2000);
-    lcd.clear();
     dry(dryTime);
-    lcd.clear();  
   }
   else {
     digitalRead(dryPin) == HIGH;
-    lcd.setCursor(0,1); lcd.print("Dry Count: ");   //11 characters   
-    lcd.setCursor(12,1);lcd.print(dryCount);
   }
       
 
   if(digitalRead(washPin) == LOW){
-    lcd.setCursor(6,3); lcd.print("ON");
-    lcd.clear();//clear screen to print new words
-    lcd.print("Wash Selected."); //2 Words, 14 characters
+    lcd.setCursor(7,3); lcd.print("ON ");
     delay(2000);
-    lcd.clear();
     wash(washTime);
-    lcd.clear(); 
   }
   else{
     digitalRead(washPin) == HIGH;
-    lcd.setCursor(0,0); lcd.print("Wash Count: ");  //12 characters   
-    lcd.setCursor(12,0);lcd.print(washCount);
   }
 
   if(washCount >= maxWash){
     lcd.clear();
-    lcd.print("wash count exceeded!");
+    lcd.print("Wash Count Exceeded!");
     delay(5000);
     maintenance();
   }
   else if(dryCount >= maxDry){
     lcd.clear();
-    lcd.print("dry count exceeded!");
+    lcd.print("Dry Count Exceeded!");
     maintenance();
   }
-
+  
 }
 
 
@@ -118,22 +98,29 @@ void loop() {
     digitalWrite(relayOne,HIGH);
     delay(time); 
     digitalWrite(relayOne, LOW);
-    washCount =washCount +1;
+    washCount =washCount + 1;
+    resetScreen(washCount, dryCount);
   }
 
   void dry(int time){
     digitalWrite(relayTwo, HIGH);
     delay(time); 
     digitalWrite(relayTwo, LOW);
-    dryCount = dryCount+1;
+    dryCount = dryCount + 1;
+    resetScreen(washCount, dryCount);
   }
 
   void maintenance(){
     lcd.clear();
-    lcd.print("WARNING! maintenance required"); 
+    lcd.setCursor(7,1); lcd.print("WARNING!"); 
+    lcd.setCursor(0,2); lcd.print("MAINTENANCE REQUIRED"); 
     
     //for reset, we ask the maintenance guy to wait 60 seconds before pressing the reset
-    delay(60000);
+    delay(30000);
+    delay(30000);
+    lcd.clear();
+    lcd.setCursor(0,0);lcd.print("Please Hold Reset");
+    delay(3000);
     if(digitalRead(resetPin)== LOW){
       resetFunct();
     }
@@ -144,22 +131,22 @@ void loop() {
     digitalWrite(relayTwo, LOW); 
     washCount =0;
     dryCount = 0;
-    resetScreen();
+    resetScreen(washCount, dryCount);
   }
 
-void resetScreen()
+void welcome(){
+  lcd.setCursor(0,0);  lcd.print("--------------------"); 
+  lcd.setCursor(0,1);  lcd.print("     WELCOME TO");
+  lcd.setCursor(0,2);  lcd.print("   SHOE - RIFFIC! ");
+  lcd.setCursor(0,3);  lcd.print("--------------------");
+}
+
+void resetScreen(int wCNT, int dCNT)
 {
-  for(int i = 0; i < 4; i++)
-  {
-    lcd.setCursor(0,i);  lcd.print("                    ");
-  }
-  Serial.println("Screen Cleared.");
-  delay(2000);
-  
+  lcd.clear();
   lcd.setCursor(0,0); lcd.print("Selection Options");
   lcd.setCursor(0,1); lcd.print("Reset: "); lcd.setCursor(7,1); lcd.print("OFF ");
-  lcd.setCursor(0,2); lcd.print("Dry: ");   lcd.setCursor(5,2); lcd.print("OFF");
-  lcd.setCursor(0,3); lcd.print("Wash: ");  lcd.setCursor(6,3); lcd.print("OFF"); 
-  
+  lcd.setCursor(0,2); lcd.print("Dry: ");   lcd.setCursor(7,2); lcd.print("OFF"); lcd.setCursor(10,2); lcd.print(" COUNT: "); lcd.setCursor(17, 2); lcd.print(dCNT);
+  lcd.setCursor(0,3); lcd.print("Wash: ");  lcd.setCursor(7,3); lcd.print("OFF"); lcd.setCursor(11,3); lcd.print("COUNT: "); lcd.setCursor(17, 3); lcd.print(wCNT);
 
 }
